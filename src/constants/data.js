d44ce105b3c7be8d4f18ba306b957213f7c2bde5
christiannaira <christianmnaira@gmail.com>
2024-02-20 17:40:46 +0800

    added data

---
+++ src/constants/data.js
@@ -0,0 +1,69 @@
+const challenges = [
+    {
+        problem: 'Building Loyal Audience',
+        solution: 'Struggling to increase your visibility and reach a wider audience',
+    },
+    {
+        problem: 'Building A Strong Personal Brand',
+        solution: 'Struggling to have a clarity and specific niche.'
+    },
+    {
+        problem: 'Creating A Consistent Contents',
+        solution: 'Spending so much time just to create contents.'
+    },
+    {
+        problem: 'Developing A Solid System & Strategy',
+        solution: 'Struggling to build a roadmap or direction to produce remarkable results.'
+    },
+    {
+        problem: 'Not Leveraging A Team To Maximize The Media',
+        solution: 'Struggle to maximize business full potential (leads & sales).'
+    },
+]
+
+const process = [
+    {
+        title: 'Organic Content Marketing',
+        info: 'We dedicate an entire team to create and repurpose your content into top-quality  videos.'
+    },
+    {
+        title: 'Analytics',
+        info: 'We analyze your contents & audience to maximize its full potential.'
+    },
+    {
+        title: 'Convert & Result',
+        info: 'We direct all views and traffic back to your main socials which skyrocket in growth, leading to a huge increase in branding, revenue and sales'
+    }
+]
+
+const testimonials = [
+    {
+        client: 'Baron Hashel',
+        profession: 'Founder & CEO',
+        words: ' We deliver fast but we also believe that quality is much more important than quantity, we assure that every penny and every dollar our clients pay us would return to them tenfold. We deliver fast but we also believe that quality is much more important than quantity, we assure that every penny and every dollar our clients pay us would return to them tenfold.'
+    },
+    {
+        client: 'Baron Hashel',
+        profession: 'Founder & CEO',
+        words: "We deliver fast but we also believe that quality is much more important than quantity, we assure that every penny and every dollar our clients pay us would return to them tenfold. We deliver fast but we also believe that.",
+    }
+]
+
+const team = [
+    {
+        member: 'Jay Marco',
+        role: 'CEO',
+    },
+    {
+        member: 'Christian Naira',
+        role: 'CTO',
+    },
+    {
+        member: 'Bryan Barnedo',
+        role: 'CMO',
+    },
+    {
+        member: 'Daniel Baria',
+        role: 'COO',
+    }
+]
